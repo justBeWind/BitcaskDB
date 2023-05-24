ae83c7691d7ad99c6d979c034c0fe0433ee81844
justBeWind <446715229@qq.com>
2023-05-24 21:14:40 +0800

    实现了对get、put、delete、listkeys、stat的http接口封装，提供用户调用

--- http/main.go
+++ http/main.go
@@ -64,10 +64,60 @@ func handleGet(writer http.ResponseWriter, request *http.Request) {
 	_ = json.NewEncoder(writer).Encode(string(value))
 }
 
+func handleDelete(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodDelete {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// 1111%!(EXTRA url.Values=map[key:[name1]])
+	// log.Printf("1111", request.URL.Query())
+	key := request.URL.Query().Get("key")
+
+	err := db.Delete([]byte(key))
+	if err != nil && err != bitcask.ErrKeyIsEmpty {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		log.Printf("failed to get kv in db %v\n", err)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(writer).Encode("OK")
+}
+
+func handleListKeys(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	keys := db.ListKeys()
+
+	writer.Header().Set("Content-Type", "application/json")
+	var result []string
+	for _, k := range keys {
+		result = append(result, string(k))
+	}
+
+	_ = json.NewEncoder(writer).Encode(result)
+}
+
+func handleStat(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	stat := db.Stat()
+	writer.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(writer).Encode(stat)
+}
+
 func main() {
 	// 注册处理方法
 	http.HandleFunc("/bitcask/put", handlePut)
 	http.HandleFunc("/bitcask/get", handleGet)
+	http.HandleFunc("/bitcask/delete", handleDelete)
+	http.HandleFunc("/bitcask/listkeys", handleListKeys)
+	http.HandleFunc("/bitcask/stat", handleStat)
 
 	// 启动HTTP服务
 	_ = http.ListenAndServe("localhost:12260", nil)
